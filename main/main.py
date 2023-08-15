b6b7b0b2b5faf208db1edbc4bdcea44519e748e4
David Garcia <120756389+AcroGG@users.noreply.github.com>
2023-08-15 14:52:59 -0500

    Update main.py

--- main/main.py
+++ main/main.py
@@ -5,26 +5,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+# Import and read in data, separate the data into training and dev data sets
 mnist = tf.keras.datasets.mnist
 (pixel_train, digit_train), (pixel_test, digit_test) = mnist.load_data()
 
+# Scaling pixel values from data to be between 0-1
 pixel_train = tf.keras.utils.normalize(pixel_train, axis=1)
 pixel_test = tf.keras.utils.normalize(pixel_test, axis=1)
 
+# Create the learning model and add neural network layers that will be applied to activation functions 
 learning_model = tf.keras.models.Sequential()
 learning_model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
 learning_model.add(tf.keras.layers.Dense(128,activation='relu'))
 learning_model.add(tf.keras.layers.Dense(128,activation='relu'))
 learning_model.add(tf.keras.layers.Dense(10,activation='softmax'))
 
+# Create a compile function and choose the optimizer
 learning_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
+# Now train the model using the data for an initial train test
 learning_model.fit(pixel_train, digit_train, epochs=5)
 
-learning_model.save('handwritten.model')
-
-learning_model  = tf.keras.models.load_model('handwritten.model')
+# Evualate the model to see how well it performs with train data cross-checked with dev data
 loss, accuracy = learning_model.evaluate(pixel_test, digit_test)
+
+# Use a file of digit image data to test the learning model
 image_number=0
 while os.path.isfile(f'digittest/{image_number}.png'):
     try:
